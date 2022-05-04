9be1acdbea7853751b39d1c1025bbf2d01881969
danielhangan <hangandaniel1@gmail.com>
2022-05-04 21:19:26 +0100

    basic user routes added

--- backend/users/api.py
+++ backend/users/api.py
@@ -1,8 +1,19 @@
 from ninja import Router
+import asyncio
 
-router = Router()
+router = Router(tags=["users"])
 
 
-@router.get("/hello")
-def hello(request):
+@router.get("/")
+async def get_all_users(request):
+    return "Hello World"
+
+
+@router.post("/")
+async def create_user(request):
+    return "Hello World"
+
+
+@router.get("/{user_email}")
+async def get_all_users(request, user_email: str):
     return "Hello World"
