bec441b136d058b4c3186fc6ee55ce21b2470c96
danielhangan <hangandaniel1@gmail.com>
2022-06-09 19:14:45 +0100

    dashboard page

---
+++ frontend/src/pages/dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import DashboardLayout from '../layouts/DashboardLayout'
+
+export default function Dashboard () {
+  return (
+      <DashboardLayout>
+        <div>dashboard</div>
+      </DashboardLayout>
+      
+  )
+}
