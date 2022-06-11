1de7bfa6a66b8c7949e3723e36daa017aab4d525
danielhangan <hangandaniel1@gmail.com>
2022-06-11 22:02:29 +0100

    login

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -11,11 +11,5 @@ export default function Login ({ providers }) {
 
 
 export async function getServerSideProps(ctx) {
-  const session = await getSession()
-  console.log(session)
-  await axios.get(`${process.env.BASE_URL}/users/profile/${session?.user?.email}`)
-    .then((data) => {
-      console.log(data.text)
-    })
   return { props: { providers: await getProviders()}}
 }
